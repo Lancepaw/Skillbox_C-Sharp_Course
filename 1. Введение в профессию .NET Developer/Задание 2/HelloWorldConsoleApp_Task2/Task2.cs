eb0313422a68cc4d5dc5689d67773a4a9ca4d90f
Lancepaw <pavelko93@gmail.com>
2022-02-21 21:31:55 +0300

    Добавил Задание 2

---
+++ 1. Введение в профессию .NET Developer/Задание 2/HelloWorldConsoleApp_Task2/Task2.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using static System.Console;
+
+namespace HelloWorldConsoleApp {
+
+public static class Task2 {
+
+static void Main()
+{	Write( "Hello " );
+	Write( "World" );
+	Write( "!!!" );
+	ReadLine();
+}
+}}
