5c4a5f4e9c2242a2d974ad52f11df89b22a877f5
Lancepaw <pavelko93@gmail.com>
2022-03-02 12:48:46 +0300

    Добавил практическое задание №2

---
+++ 2. Базовые возможности C#/Задание 1/ConsoleApp_task_2_1/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+using static System.Console;
+
+namespace ConsoleApp_Task_2_1{
+static class Program{
+
+static void Main()
+{	int age;
+	string fullName, mail;
+	double programming, math, physics;
+	
+	mail     = "pavelko93@gmail.com"       ;
+	fullName = "Кожевников Павел Андреевич";
+
+	age         = 28 ;
+	math        = 4.8;
+	physics     = 4.3;
+	programming = 5.0;
+
+	WriteLine( $"Ф.И.О: { fullName };\n\rВозраст: { age };\n\rE-mail: { mail };\n\rБалл по программированию: { programming.ToString("0.0") };\n\rБалл по математике: { math };\n\rБалл по физике: { physics };" );
+	ReadLine();
+}
+	
+}}
---
+++ 2. Базовые возможности C#/Задание 2/ConsoleApp_task_2_2/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+using static System.Console;
+
+namespace ConsoleApp_Task_2_2{
+static class Program{
+
+static void Main()
+{	int age;
+	string fullName, mail;
+	double programming, math, physics, summary, average;
+	
+	mail     = "pavelko93@gmail.com"       ;
+	fullName = "Кожевников Павел Андреевич";
+
+	age         = 28 ;
+	math        = 4.8;
+	physics     = 4.3;
+	programming = 5.0;
+
+	summary =  math + physics + programming;
+	average = summary / 3;
+
+	WriteLine( $"Ф.И.О: { fullName };\n\rВозраст: { age };\n\rE-mail: { mail };\n\rБалл по программированию: { programming.ToString("0.0") };\n\rБалл по математике: { math };\n\rБалл по физике: { physics };" );
+	ReadKey();
+	WriteLine( $"\rОбщий балл: { summary.ToString("0.0") };" );
+	ReadKey();
+	WriteLine( $"\rСредний балл: { average.ToString("0.0") };" );
+}
+	
+}}
