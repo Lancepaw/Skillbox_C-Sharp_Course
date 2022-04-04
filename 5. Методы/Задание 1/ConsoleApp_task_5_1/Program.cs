024211baf9979fd760a67c3346a0f5f6837b57a1
Lancepaw <pavelko93@gmail.com>
2022-04-04 20:16:00 +0300

    Выложил практическое задание к блоку 5

---
+++ 5. Методы/Задание 1/ConsoleApp_task_5_1/Program.cs
@@ -0,0 +1,52 @@
+﻿using System;
+
+using static System.Console;
+
+namespace ConsoleApp_task_5_1{
+static class Program{
+
+static void Main()
+{	string   input   ;
+	string[] splitted;
+
+	WriteLine( "Введите предложение.\n" );
+	input = ReadLine();
+
+	WriteLine( $"\nРазбивка приложения самодельным способом:\n" );
+	PrintLineByLine( SplitStringManually( input ) );
+
+	WriteLine( "Введите ещё одно предложение.\n" );
+	input = ReadLine();
+
+	WriteLine( $"\nРазбивка приложения с использованием библиотечного метода:\n" );
+	PrintLineByLine( SplitStringUsingLibrary( input ) );
+	ReadLine();
+}
+
+static private string[] SplitStringManually( string input )
+{	List< string > res = new List< string >();
+
+	while( true )
+	{	int tmp = input.IndexOf( ' ' );
+
+		if( tmp > 0 )
+		{	res.Add( input.Substring(0, tmp) );
+			input = input.Substring( tmp + 1 );
+		}
+		else
+		{	res.Add( input );
+			break;  
+		}
+	}
+
+	return res.ToArray();
+}
+static private string[] SplitStringUsingLibrary( string input )
+{	return input.Split(' ');  }
+
+static private void PrintLineByLine( string[] line)
+{	for( int i = 0; i < line.Length; i++ )
+	{	WriteLine( $"{ line[i] }\n" );	}
+}
+	
+}}
---
+++ 5. Методы/Задание 2/ConsoleApp_task_5_2/Program.cs
@@ -0,0 +1,56 @@
+﻿using System;
+
+using static System.Console;
+
+namespace ConsoleApp_task_5_2{
+static class Program{
+
+static void Main()
+{	string   input;
+	string[] splitted;
+
+	WriteLine( "Введите предложение.\n" );
+	input = ReadLine();
+
+	WriteLine( $"\nИнвертированное предложение:\n" );
+	InvertAndPrintString( input );
+	ReadLine();
+}
+
+static private void InvertAndPrintString( string input )
+{	string   invertedString;
+	string[] splittedString;
+
+	invertedString = "";
+	splittedString = SplitStringManually( input );
+
+	for( int i = splittedString.Length-1; i >= 0 ; i-- )
+	{	invertedString += splittedString[ i ];
+	
+		if( i != 0 )
+			invertedString += ' ';
+	}
+
+	WriteLine( invertedString );
+}
+
+static private string[] SplitStringManually( string input )
+{	List< string > res = new List< string >();
+
+	while( true )
+	{	int tmp = input.IndexOf( ' ' );
+
+		if( tmp > 0 )
+		{	res.Add( input.Substring(0, tmp) );
+			input = input.Substring( tmp + 1 );
+		}
+		else
+		{	res.Add( input );
+			break;  
+		}
+	}
+
+	return res.ToArray();
+}
+	
+}}
